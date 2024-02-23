9a42c5186f241f7973ea0a2f6ad9af69fcbcf286
pya-h <p4ya.py@gmail.com>
2024-02-23 21:16:47 +0330

    database completed; and shortend

--- v2/db/vip_interface.py
+++ v2/db/vip_interface.py
@@ -1,6 +1,6 @@
 import sqlite3
 from tools import manuwriter
-from tools.mathematix import after_n_months
+from tools.mathematix import after_n_months, tz_today
 from time import time
 from datetime import datetime
 
@@ -8,43 +8,23 @@ from datetime import datetime
 class VIPDatabaseInterface:
     _instance = None
     TABLE_ACCOUNTS = "accounts"
-    ACCOUNT_ID = 'id'
-    ACCOUNT_CURRENCIES = 'currencies'
-    ACCOUNT_CRYPTOS = 'cryptos'
-    ACCOUNT_LAST_INTERACTION = 'last_interaction'
-    ACCOUNT_LANGUAGE = 'language'
     DATE_FORMAT = '%Y-%m-%d'
-    ACCOUNT_VIP_END_DATE= 'vip_end_date'  # verified as vip
-    ACCOUNT_VIP_PLAN_ID = 'vip_plan_id'
+    ACCOUNT_COLUMNS = (ACCOUNT_ID, ACCOUNT_CURRENCIES, ACCOUNT_CRYPTOS, ACCOUNT_LAST_INTERACTION, ACCOUNT_VIP_END_DATE, ACCOUNT_VIP_PLAN_ID, ACCOUNT_LANGUAGE) =\
+        ('id', 'currencies', 'cryptos', 'last_interaction', 'vip_end_date', 'vip_plan_id', 'language')
     ACCOUNT_ALL_FIELDS = f'({ACCOUNT_ID}, {ACCOUNT_CURRENCIES}, {ACCOUNT_CRYPTOS}, {ACCOUNT_LAST_INTERACTION}, {ACCOUNT_VIP_END_DATE}, {ACCOUNT_VIP_PLAN_ID}, {ACCOUNT_LANGUAGE})'
 
     TABLE_CHANNELS = "channels"  # channels to be scheduled
-    CHANNEL_ID = "id"
-    CHANNEL_INTERVAL = "interval"
-    CHANNEL_OWNER_ID = "owner_id"  # ref to account
-    CHANNEL_NAME = "name"
-    CHANNEL_LAST_POST_TIME = "last_post_time"
-    CHANNEL_TITLE = "title"
-    CHANNEL_ALL_FIELDS = f'({CHANNEL_ID}, {CHANNEL_NAME}, {CHANNEL_TITLE}, {CHANNEL_OWNER_ID}, {CHANNEL_INTERVAL}, {CHANNEL_LAST_POST_TIME})'
+    CHANNELS_COLUMNS = (CHANNEL_ID, CHANNEL_NAME, CHANNEL_TITLE, CHANNEL_OWNER_ID, CHANNEL_INTERVAL, CHANNEL_LAST_POST_TIME) =\
+        ("id", "name", "title", "owner_id", "interval", "last_post_time")
 
     TABLE_PAYMENTS = "payments"
-    PAYMENT_ID = "id"
-    PAYMENT_ORDER_ID = "order_id"
-    PAYMENT_STATUS = "status"
-    PAYMENT_AMOUNT = "amount"
-    PAYMENT_CURRENCY = "CURRENCY"
-    PAYMENT_PAID_AMOUNT = "paid_amount"
-    PAYMENT_PAID_CURRENCY = "paid_CURRENCY"
-    PAYMENT_CHATID = "chat_id"
-    PAYMENT_VIP_PLAN_ID = "vip_plan_id"
-    PAYMENT_NETWORK = "network"
-    
+    PAYMENT_COLUMNS = (PAYMENT_ID, PAYMENT_CHATID, PAYMENT_ORDER_ID, PAYMENT_STATUS, PAYMENT_AMOUNT, PAYMENT_CURRENCY, PAYMENT_PAID_AMOUNT, PAYMENT_PAID_CURRENCY, PAYMENT_VIP_PLAN_ID, PAYMENT_MODIFIED_ON) =\
+        ("order_id", "chat_id", "id", "status", "amount", "currency", "paid_amount", "paid_CURRENCY", "vip_plan_id", "network", "modified")
+        
     TABLE_VIP_PLANS = "vip_plans"
-    VIP_PLAN_ID = "id"
-    VIP_PLAN_DESCRIPTION = "description"
-    VIP_PLAN_TITLE = "title"
-    VIP_PLAN_DURATION = "duration" # in months
-    VIP_PLAN_LEVEL = "level"
+    VIP_PLANS_COLUMNS = (VIP_PLAN_ID, VIP_PLAN_DESCRIPTION, VIP_PLAN_TITLE, VIP_PLAN_DURATION, VIP_PLAN_LEVEL) =\
+        ("id", "description", "title", "duration", "level")
+
 
     @staticmethod
     def Get():
@@ -90,7 +70,7 @@ class VIPDatabaseInterface:
 
             # Table payments existence check
             if not cursor.execute(f"SELECT name from sqlite_master WHERE name='{VIPDatabaseInterface.TABLE_PAYMENTS}'").fetchone():
-                query = f"CREATE TABLE {VIPDatabaseInterface.TABLE_PAYMENTS} ({VIPDatabaseInterface.PAYMENT_ID} INTEGER PRIMARY KEY, " +\
+                query = f"CREATE TABLE {VIPDatabaseInterface.TABLE_PAYMENTS} ({VIPDatabaseInterface.PAYMENT_ID} INTEGER, " +\
                     f"{VIPDatabaseInterface.PAYMENT_ORDER_ID} INTEGER, {VIPDatabaseInterface.PAYMENT_CHATID} INTEGER, " +\
                     f"{VIPDatabaseInterface.PAYMENT_AMOUNT} REAL, {VIPDatabaseInterface.PAYMENT_CURRENCY} TEXT, " +\
                     f"{VIPDatabaseInterface.PAYMENT_PAID_AMOUNT} REAL, {VIPDatabaseInterface.PAYMENT_PAID_CURRENCY} TEXT, " +\
@@ -119,7 +99,8 @@ class VIPDatabaseInterface:
         if not account:
             raise Exception("You must provide an Account to save")
         try:
-            query = f"INSERT INTO {VIPDatabaseInterface.TABLE_ACCOUNTS} {VIPDatabaseInterface.ACCOUNT_ALL_FIELDS} VALUES (?, ?, ?, ?, ?, ?, ?)"
+            columns = ', '.join(VIPDatabaseInterface.ACCOUNT_COLUMNS)
+            query = f"INSERT INTO {VIPDatabaseInterface.TABLE_ACCOUNTS} ({columns}) VALUES (?, ?, ?, ?, ?, ?, ?)"
             self.execute(False, query, account.chat_id, account.str_desired_currencies(), account.str_desired_coins(), account.last_interaction.strftime(VIPDatabaseInterface.DATE_FORMAT), \
                                    account.vip_end_date, account.vip_plan_id, account.language)
             manuwriter.log(f"New account: {account} saved into vip database successfully.", category_name=f'vip_info')
@@ -132,12 +113,7 @@ class VIPDatabaseInterface:
         return accounts[0] if accounts else None
 
     def get_all(self, column: str=ACCOUNT_ID) -> list:
-        connection = sqlite3.connect(self._name)
-        cursor = connection.cursor()
-        cursor.execute(f"SELECT ({column}) FROM {VIPDatabaseInterface.TABLE_ACCOUNTS}")
-        rows = cursor.fetchall()
-        cursor.close()
-        connection.close()
+        rows = self.execute(True, f"SELECT ({column}) FROM {VIPDatabaseInterface.TABLE_ACCOUNTS}")
         if column == VIPDatabaseInterface.ACCOUNT_LAST_INTERACTION:
             return [datetime.strptime(row[0], VIPDatabaseInterface.DATE_FORMAT) if row[0] else None for row in rows]
         return [row[0] for row in rows] # just return a list of ids
@@ -147,12 +123,14 @@ class VIPDatabaseInterface:
         cursor = connection.cursor()
         cursor.execute(f"SELECT * FROM {VIPDatabaseInterface.TABLE_ACCOUNTS} WHERE {VIPDatabaseInterface.ACCOUNT_ID}=? LIMIT 1", (account.chat_id, ))
         if cursor.fetchone(): # if account with his chat id has been saved before in the database
-            FIELDS_TO_SET = f'{VIPDatabaseInterface.ACCOUNT_CURRENCIES}=?, {VIPDatabaseInterface.ACCOUNT_CRYPTOS}=?, {VIPDatabaseInterface.ACCOUNT_LAST_INTERACTION}=?, {VIPDatabaseInterface.ACCOUNT_VIP_END_DATE}=?, {VIPDatabaseInterface.ACCOUNT_VIP_PLAN_ID}=?, {VIPDatabaseInterface.ACCOUNT_LANGUAGE}=?'
-            cursor.execute(f'UPDATE {VIPDatabaseInterface.TABLE_ACCOUNTS} SET {FIELDS_TO_SET} WHERE {VIPDatabaseInterface.ACCOUNT_ID}=?', \
+            columns_to_set = ', '.join([f'{field}=?' for field in VIPDatabaseInterface.ACCOUNT_COLUMNS[1:]])
+            
+            cursor.execute(f'UPDATE {VIPDatabaseInterface.TABLE_ACCOUNTS} SET {columns_to_set} WHERE {VIPDatabaseInterface.ACCOUNT_ID}=?', \
                 (account.str_desired_currencies(), account.str_desired_coins(), account.last_interaction.strftime(VIPDatabaseInterface.DATE_FORMAT), \
                  account.vip_end_date.strftime(VIPDatabaseInterface.DATE_FORMAT) if account.vip_end_date else None, account.vip_plan_id, account.language, account.chat_id))
         else:
-            cursor.execute(f"INSERT INTO {VIPDatabaseInterface.TABLE_ACCOUNTS} {VIPDatabaseInterface.ACCOUNT_ALL_FIELDS} VALUES (?, ?, ?, ?, ?, ?, ?)", \
+            columns = ', '.join(VIPDatabaseInterface.ACCOUNT_COLUMNS)
+            cursor.execute(f"INSERT INTO {VIPDatabaseInterface.TABLE_ACCOUNTS} ({columns}) VALUES (?, ?, ?, ?, ?, ?, ?)", \
                 (account.chat_id, account.str_desired_currencies(), account.str_desired_coins(), account.last_interaction.strftime(VIPDatabaseInterface.DATE_FORMAT), \
                  account.vip_end_date.strftime(VIPDatabaseInterface.DATE_FORMAT) if account.vip_end_date else None, account.vip_plan_id,  account.language))
             manuwriter.log("New account started using this bot with chat_id=: " + account.__str__(), category_name=f'vip_info')
@@ -167,19 +145,19 @@ class VIPDatabaseInterface:
             str_vip_end_date, vip_plan_id, account.chat_id)
         manuwriter.log(f"Account with chat_id={account.chat_id} has extended its vip previllages until {str_vip_end_date}")
 
-
     def plan_channel(self, owner_chat_id: int, channel_id: int, channel_name: str, interval: int, channel_title: str):
         connection = sqlite3.connect(self._name)
         cursor = connection.cursor()
         cursor.execute(f"SELECT * FROM {VIPDatabaseInterface.TABLE_CHANNELS} WHERE {VIPDatabaseInterface.CHANNEL_ID}=? LIMIT 1", (channel_id, ))
         now_in_minutes = time() // 60
         if cursor.fetchone(): # if account with his chat id has been saved before in the database
-            FIELDS_TO_SET = f'{VIPDatabaseInterface.CHANNEL_OWNER_ID}=?, {VIPDatabaseInterface.CHANNEL_INTERVAL}=?, {VIPDatabaseInterface.CHANNEL_NAME}=?, {VIPDatabaseInterface.CHANNEL_TITLE}=?, {VIPDatabaseInterface.CHANNEL_LAST_POST_TIME}=?'
-            cursor.execute(f'UPDATE {VIPDatabaseInterface.TABLE_CHANNELS} SET {FIELDS_TO_SET} WHERE {VIPDatabaseInterface.CHANNEL_ID}=?', \
-                (owner_chat_id, interval, channel_name, channel_title, now_in_minutes, channel_id))
+            columns_to_set = ', '.join([f'{field}=?' for field in VIPDatabaseInterface.CHANNELS_COLUMNS[1:]])
+            cursor.execute(f'UPDATE {VIPDatabaseInterface.TABLE_CHANNELS} SET {columns_to_set} WHERE {VIPDatabaseInterface.CHANNEL_ID}=?', \
+                (channel_name, channel_title, owner_chat_id, interval, now_in_minutes, channel_id))
             manuwriter.log(f"Channel with the id of [{channel_id}, {channel_name}] has been RE-planned by owner_chat_id=: {owner_chat_id}", category_name='vip_info')
         else:
-            cursor.execute(f"INSERT INTO {VIPDatabaseInterface.TABLE_CHANNELS} {VIPDatabaseInterface.CHANNEL_ALL_FIELDS} VALUES (?, ?, ?, ?, ?, ?)", \
+            columns = ', '.join(VIPDatabaseInterface.CHANNELS_COLUMNS)
+            cursor.execute(f"INSERT INTO {VIPDatabaseInterface.TABLE_CHANNELS} ({columns}) VALUES (?, ?, ?, ?, ?, ?)", \
                 (channel_id, channel_name, channel_title, owner_chat_id, interval, now_in_minutes))
             manuwriter.log(f"New channel with the id of [{channel_id}, {channel_name}] has benn planned by owner_chat_id=: {owner_chat_id}", category_name='vip_info')
         connection.commit()
@@ -217,8 +195,37 @@ class VIPDatabaseInterface:
         '''Delete channel and its planning'''
         self.execute(False, f"DELETE FROM {VIPDatabaseInterface.TABLE_CHANNELS} WHERE {VIPDatabaseInterface.CHANNEL_ID} = ?", channel_id)
 
-    def define_plan(self, title: str, description: str, duration_in_months: int, level: int = 1):
-        pass
+    def define_vip_plan(self, title: str, description: str, duration_in_months: int, vip_level: int = 1):
+        fields = ', '.join(VIPDatabaseInterface.VIP_PLANS_COLUMNS[1:])
+        self.execute(f"INSERT INTO {VIPDatabaseInterface.TABLE_VIP_PLANS} ({fields}) VALUES (?, ?, ?, ?)", description, title, duration_in_months, vip_level)
+
+    def get_vip_plan(self, vip_plan_id: int):
+        vplans = self.execute(True, f"SELECT * FROM {VIPDatabaseInterface.TABLE_VIP_PLANS} WHERE {VIPDatabaseInterface.VIP_PLAN_ID}=? LIMIT 1", vip_plan_id)
+        return vplans[0] if vplans else None
+    
+    def update_payment(self, payment):
+        connection = sqlite3.connect(self._name)
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT * FROM {VIPDatabaseInterface.TABLE_PAYMENTS} WHERE {VIPDatabaseInterface.PAYMENT_ORDER_ID}=? LIMIT 1", (payment.order_id, ))
+        now = tz_today()
+        if cursor.fetchone(): # if account with his chat id has been saved before in the database
+            columns = ', '.join(f"{field}=?" for field in VIPDatabaseInterface.PAYMENT_COLUMNS[2:])
+            cursor.execute(f'UPDATE {VIPDatabaseInterface.TABLE_PAYMENTS} SET {columns} WHERE {VIPDatabaseInterface.PAYMENT_ORDER_ID}=?', \
+                (payment.id, payment.status, payment.amount, payment.currency, payment.paid_amount, payment.paid_currency, payment.payer_chat_id, payment.vip_plan.id, \
+                    payment.network, now.strftime(VIPDatabaseInterface.DATE_FORMAT + " %H:%M"), payment.order_id))
+            manuwriter.log(f"Payment with order_id of {payment.order_id}, and payment id of {payment.id} status changed to {payment.status}", category_name='payments')
+        else:
+            columns = ', '.join(VIPDatabaseInterface.PAYMENT_COLUMNS)
+            cursor.execute(f"INSERT INTO {VIPDatabaseInterface.TABLE_PAYMENTS} ({columns}) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", \
+                (payment.order_id, payment.payer_chat_id, payment.id, payment.status, payment.amount, payment.currency, payment.paid_amount, payment.paid_currency, payment.vip_plan.id, payment.network, now.strftime(VIPDatabaseInterface.DATE_FORMAT + " %H:%M")))
+            manuwriter.log(f"New payment with the id of {payment.id} and order_id of {payment.order_id}, and status of {payment.status} added for user with chatid =: {payment.payer_chat_id}", category_name='payments')
+        connection.commit()
+        cursor.close()
+        connection.close()
+    
+    def get_payment(self, order_id):
+        return self.execute(True, f"SELECT * from {VIPDatabaseInterface.TABLE_PAYMENTS} WHERE {VIPDatabaseInterface.PAYMENT_ORDER_ID}=? ", order_id)
+            
     def __init__(self, name="vip_data.db"):
         self._name = name
         self.setup()
