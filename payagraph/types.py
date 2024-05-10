52fdf0c7028d07150ed975661e5d566c7fa640ee
pya-h <p4ya.py@gmail.com>
2024-05-10 11:53:58 +0330

    [del] payagraph

--- payagraph/types.py
+++
@@ -1,8 +0,0 @@
-
-class CanBeKeyboardItemInterface:
-    '''This is used for for example passing argument to InlineKeyboard.Arrange'''
-    def title(self) -> str:
-        pass
-    def value(self) -> int:
-        pass
-
