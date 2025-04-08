0d4d483c7644d6d3d085256700dee00be42355e6
pya-h <p4ya.py@gmail.com>
2025-04-08 20:32:37 +0330

    add a new util script to fix account old desired tokens bug

---
+++ tests/desired-fixer.py
@@ -0,0 +1,27 @@
+from models.account import Account
+from api.crypto_service import CoinMarketCap
+from api.currency_service import SourceArena
+
+
+chats = Account.Everybody()
+
+x = CoinMarketCap()
+y = SourceArena()
+for chat_id in chats:
+    try:
+        acc = Account.Get(chat_id)
+        changed = False
+        if acc.desired_coins:
+            new_cryptos = list(filter(lambda token: token in CoinMarketCap.CoinsInPersian, acc.desired_coins))
+            changed = len(new_cryptos) != len(acc.desired_coins)
+            acc.desired_coins = new_cryptos
+        if acc.desired_currencies:
+            new_currs = list(filter(lambda token: token in SourceArena.CurrenciesInPersian, acc.desired_currencies))
+            if not changed:
+                changed = len(new_currs) != len(acc.desired_currencies)
+            acc.desired_currencies = new_currs
+        if changed:
+            acc.save()
+            print(acc.chat_id, 'Fixed.')
+    except Exception as x:
+        print(x)
